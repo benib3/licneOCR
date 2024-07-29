8c116065844a62146d8e324328b2a7795c32d354
benib3 <brate.tebra@live.com>
2024-07-29 14:55:59 +0200

    fixed requirments and added https to be implmented

--- app.py
+++ app.py
@@ -144,4 +144,6 @@ def upload_file():
     return jsonify(info_dict)
 
 if __name__ == '__main__':
+  #  context = ('cert.pem', 'key.pem') #TODO: Add your own SSL certificate and key
+  #   app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=context)
     app.run(debug=True, host='0.0.0.0', port=5000)
